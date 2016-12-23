b687dbccf686b3f0099f35f1cabce5abc2b5914f
Cliffe Hodgkinson <cliffe@resounding.ca>
2016-12-22 21:24:37 -0500

    Fix authentication issues with replication by using API key

--- src/resources/services/database.ts
+++ src/resources/services/database.ts
@@ -15,30 +15,45 @@ export class Database {
         this.events.subscribe(Authentication.AuthenticatedEvent, this.init.bind(this));
     }
 
-    init() {
+    init(localOps?:PouchDB.Configuration.DatabaseConfiguration, remoteOps?:PouchDB.Configuration.DatabaseConfiguration) {
         if (localDB === null) {
-            localDB = new PouchDB(this.config.app_database_name);
-            if(Configuration.isDebug()) {
-                //PouchDB.debug.enable('*');
-                //PouchDB.debug.disable();
+            if(localOps) {
+                localDB = new PouchDB(this.config.app_database_name, localOps);
+            } else {
+                localDB = new PouchDB(this.config.app_database_name);
             }
         }
 
         if (this.auth.isAuthenticated()) {
-            const userInfo = this.auth.userInfo;
-
-            remoteDB = new PouchDB(this.config.remote_database_name, {
+            const userInfo = this.auth.userInfo,
+                headers = {Authorization: userInfo.basicAuth},
+                opts = {
                 skip_setup: true,
                 auth: {username: userInfo.name, password: userInfo.password}
-            });
+                };
+            if(remoteOps) {
+                _.extend(opts, remoteOps);
+            }
 
-            localDB.sync(remoteDB, {live: true})
+            remoteDB = new PouchDB(this.config.remote_database_name, opts);
+
+            const sync = localDB.sync(remoteDB, {live: true})
                 .on('complete', () => {
                     log.debug('Sync complete');
                 })
                 .on('error', err => {
                     log.error('Sync error');
                     log.error(err);
+                    const values = _.values(err);
+                    // this happens on iOS 10/Safari. Use the API keys...
+                    if(values.indexOf('_reader access is required for this request') !== -1) {
+                        try {
+                            sync.cancel();
+                        } catch (e) { }
+
+                        localDB = null;
+                        this.init(undefined, this.apiKeyOptions);
+                    }
                 })
                 .on('change', change => {
                     log.info('Sync change');
@@ -72,6 +87,18 @@ export class Database {
     //noinspection JSMethodCanBeStatic
     get db() {
         return localDB;
+    }
+
+    get apiKeyOptions():PouchDB.Configuration.DatabaseConfiguration {
+        return Configuration.isDebug() ?
+        {
+            skip_setup: true,
+            auth: {username: 'blosterionsionatteracanc', password: '69cae2ccabb512fbdbb35da517c1a64071deb07f'}
+        } :
+        {
+            skip_setup: true,
+            auth: {username: 'anytombsoloventmeatterse', password: '1f42225b5e1328fc400e407ce89f253eb834a904'}
+        };
     }
 
     static OrdersSyncChangeEvent:string = 'OrdersSyncChangeEvent';
