c86beb501204f16a0bc67a5060475ba69c974122
Cliffe Hodgkinson <cliffe@resounding.ca>
2018-03-29 15:09:08 -0400

    Migrate to IBM Bluemix Cloudant

--- src/resources/services/configuration.ts
+++ src/resources/services/configuration.ts
@@ -1,13 +1,13 @@
 export class Configuration {
 
     constructor() {
-        this.remote_database_name = Configuration.isDebug() ? `${this.remote_server}/boekestyn-clean` : `${this.remote_server}/boekestyn`;
+        this.remote_database_name = Configuration.isDebug() ? `${this.remote_server}/boekestyn-test` : `${this.remote_server}/boekestyn`;
     }
 
     app_database_name:string = 'GreenRP';
     app_root:string = 'resources/views/app';
     login_root:string = 'resources/views/login';
-    remote_server:string = 'https://resounding.cloudant.com';
+    remote_server:string = 'https://7bff221b-c372-4033-bf6c-59d2e859a520-bluemix.cloudant.com';
     remote_database_name:string;
 
     static isDebug():boolean {
